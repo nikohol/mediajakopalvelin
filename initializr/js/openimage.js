5017f7dbc7303c5f65677ef7b0f84ab29f40ecdb
Mtk112 <impanaza@hotmail.com>
2016-11-29 12:45:23 +0200

    dropdown
    jeje

---
+++ initializr/js/openimage.js
@@ -0,0 +1,36 @@
+/* var xhr = new XMLHttpRequest();
+// Create XMLHttpRequest object. Name it xhr.
+function showImages() {
+	if (this.readyState == 4 && this.status == 200) {
+		var arr = JSON.parse(this.responseText);
+		for (var i = 0; 1 < arr.length; i++) {
+			var img = document.createElement('img');
+			img src = 'img/recent' + arr[i].mediaRecent;
+			console.log(arr[0]);
+		}
+	}
+}
+xhr.open('GET', 'images.json', true);
+xhr.send */
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
