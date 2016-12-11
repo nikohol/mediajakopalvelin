2a9795a747fe6d0996fbdb1357a6122e31c6a898
Mtk112 <impanaza@hotmail.com>
2016-12-11 17:28:29 +0200

    wat
    dunno what i changed

--- initializr/MediaNetBeans/src/java/controller/Login.java
+++ initializr/MediaNetBeans/src/java/controller/Login.java
@@ -5,10 +5,58 @@
  */
 package controller;
 
+import Stuff.Validation;
+import model.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+
 /**
  *
  * @author Miikka
  */
-public class Login {
+public class Login extends HttpServlet{
     
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String user = request.getParameter("username");
+        String password = request.getParameter("pword");
+        
+        if(Validation.uValidate(user, password)){
+            // Gets serssion and sets attributes
+            HttpSession hsession = request.getSession();
+            hsession.setAttribute("connection:","true");
+            hsession.setAttribute("login:", user);
+            //creates current user
+            User current = new User();
+            
+            RequestDispatcher getUser = request.getRequestDispatcher("GetUser.do");
+            getUser.include(request,response);
+        }
+        else{
+            String error = "The username/password combination is invalid.";
+            request.setAttribute("login failed:", error);
+            RequestDispatcher reqdp = request.getRequestDispatcher("login");
+            reqdp.forward(request, response);
+        }
+}
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
 }
