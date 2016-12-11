5cac07a137c8c8613fc1ae43e788d865849379d2
Mtk112 <impanaza@hotmail.com>
2016-12-11 17:26:32 +0200

    loginiä lisää
    validation

--- initializr/MediaNetBeans/src/java/Stuff/Validation.java
+++ initializr/MediaNetBeans/src/java/Stuff/Validation.java
@@ -5,10 +5,35 @@
  */
 package Stuff;
 
+import java.sql.*;
+
 /**
  *
  * @author Miikka
  */
+//User validation
 public class Validation {
+    public static boolean uValidate(String username, String pword){
+        boolean uv = false;
+        try{
+        //DB connection 
+        String connectionURL = "jdbc:mysql://10.114.32.81:3306/Sharing"; 
+        Connection connection = null;
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        connection = DriverManager.getConnection(connectionURL, "miikkatk", "seagh3id");
+        
+        PreparedStatement ps =connection.prepareStatement
+                             ("SELECT * FROM user WHERE username=? AND pword=?");
+        ps.setString(1, username);
+        ps.setString(2, pword);
+        ResultSet rs = ps.executeQuery();
+        uv = rs.next();
+        
+      }catch(Exception e)
+        {
+          e.printStackTrace();
+      }
+         return uv;  
+    }
     
 }
