0656d2e88156e7295f36e2d5d4219679f910247b
Mtk112 <impanaza@hotmail.com>
2016-12-11 18:19:08 +0200

    logout
    hups

--- initializr/MediaNetBeans/src/java/controller/Logout.java
+++ initializr/MediaNetBeans/src/java/controller/Logout.java
@@ -5,10 +5,42 @@
  */
 package controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
 /**
  *
  * @author Miikka
  */
-public class Logout {
+public class Logout extends HttpServlet{
     
+    
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+        session.invalidate();
+        
+    }
+    
+        /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
 }
