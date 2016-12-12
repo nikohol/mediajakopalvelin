0b112760523dd66c5c09b96e1dfb8632f876fee0
Mtk112 <impanaza@hotmail.com>
2016-12-12 16:30:32 +0200

    cleanup
    heyhey

--- initializr/MediaNetBeans/web/js/upload.js
+++ initializr/MediaNetBeans/web/js/upload.js
@@ -1,20 +1,4 @@
-/* var xhr = new XMLHttpRequest();
-// Create XMLHttpRequest object. Name it xhr.
-function showImages() {
-	if (this.readyState == 4 && this.status == 200) {
-		var arr = JSON.parse(this.responseText);
-		for (var i = 0; 1 < arr.length; i++) {
-			var img = document.createElement('img');
-			img src = 'img/recent' + arr[i].mediaRecent;
-			console.log(arr[0]);
-		}
-	}
-}
-xhr.open('GET', 'images.json', true);
-xhr.send */
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
