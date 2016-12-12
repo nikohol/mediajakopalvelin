3d223271e5cf9d4ff71decdebfa5dcbd9b01fc1b
nikohol <nikohol@metropolia.fi>
2016-12-12 19:24:13 +0200

    JavaScript cleanup

--- initializr/MediaNetBeans/web/js/main.js
+++ initializr/MediaNetBeans/web/js/main.js
@@ -90,3 +90,77 @@ $(document).ready(function (event) {
 		
 	}
 */
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+// Login validation
+
+function validateLogin(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+var realusername = "";
+var realpword = "";
+var user = fetch('getUserJson(@FormParam("username") String username))').then(function(response) {
+    // Convert to JSON
+    return response.json();
+}).then(function(j) {
+    // Yay, `j` is a JavaScript object
+    realuname = user.username;
+	realpword = user.pword;
+});
+
+if ( username == realuname && password == realpword){
+alert ("Welcome "+username+", enjoy your stay!");
+window.location = "index.html"; // Redirecting to main page.
+return false;
+}
+else if(realuname == 'null'){
+// turn username form red
+alert("Login failed! Check your username and/or password!");
+}
+else{
+	// turn password from red!
+}
+}
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(200);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
